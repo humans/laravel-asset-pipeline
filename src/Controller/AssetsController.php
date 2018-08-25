5699075089df16800ea8596da17de44a19230e7f
Jaggy Gauran <jaggy@artisan.studio>
2018-08-25 21:30:45 +0800

    Fetch the image and return the asset from the controller.

--- src/Controller/AssetsController.php
+++ src/Controller/AssetsController.php
@@ -6,5 +6,17 @@ class AssetsController extends \Illuminate\Routing\Controller
 {
     public function show($asset = null)
     {
+        $asset = $this->asset($asset);
+
+        if (! file_exists($asset)) {
+            abort(404);
+        }
+
+        return response()->file($asset);
+    }
+
+    private function asset($asset)
+    {
+        return str_finish(config('laravel-asset-pipeline.path'), '/') . $asset;
     }
 }
